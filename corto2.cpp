971b860f77c392a59e925ec2503aecccd976b00b
Andrewgx123 <00184825@uca.edu.sv>
2025-05-13 10:49:01 -0600

    corto2

---
+++ corto2.cpp
@@ -0,0 +1,138 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int edad,genero,estilos;
+float preciofinal
+float cartera=200
+cout <<"bienvenido a nuestra tienda en linea"<< endl;
+cout <<"ingrese su edad"<< endl;
+cout <<"de 0 a 12 años" << endl;
+cout <<"de 13 a 17 años"<< endl;
+cout <<"de 18 a 64 años"<< endl;
+cout <<"de 65 años en adelante"<< endl;
+
+cin >> edad;
+switch(edad){
+  case 1:
+  cout <<"su talla seria 34"<< endl;
+  break;
+
+  case 2:
+  cout <<"su talla seria 38"<< endl;
+  break;
+
+  case 3:
+  cout<<"su talla seria 40"<< endl;
+  break;
+  
+  case 3:
+  cout<<"su talla seria 42"
+}
+
+cout<<"ya selccionado tu talla indicanos cual es tu genero"<<endl;
+
+cin >> genero;
+switch(genero){
+    case 1:
+    cout<<"para hombre"<< endl;
+    break;
+
+    case 2:
+    cout<<"para mujer"<< endl;
+    break;
+
+    case 3:
+    cout <<"mostrar todo"<< endl;
+    break;
+}
+
+cout<<"ingresa tu estilo y tu saldo de dinero(son 200$)"<<endl;
+cin >> estilos;
+switch (estilos){
+    case 1:
+    cout <<"Zapato Deportivo hombre: $80 (descuento del 20%, precio con descuento $64)."<< endl;
+    break;
+
+    case 2:
+    cout <<"Zapato Formal hombre: $120 (descuento del 20%, precio con descuento $96)."<< endl;
+    break;
+
+    case 3:
+    cout <<"Zapato Premium hombre: $250 (descuento del 20%, precio con descuento $200)."<< endl;
+    break;
+
+    case 4:
+    cout <<"zapato casual(mujer):70$(descuento del 15%, precio con descuento $59.5) "<< endl;
+    break;
+
+    case 5:
+    cout <<"zapato elegante(mujer):100$(descuento del 15%, precio con descuento $85)"<<endl;
+    break;
+
+    case 6:
+    cout <<"zapato premium(mujer):250$(descuento del 15%, precio con descuento $212)"<<endl;
+    break;
+}
+
+cin >> preciofinal;
+switch (preciofinal){
+    
+
+    case 1:
+    cout <<"su descuento fue $64"<<endl;
+    break;
+
+    case 2:
+    cout <<" su descuento fue de $96"<< endl;
+    break;
+
+    case 3:
+    cout <<"su descuento fue de $96"<< endl;
+    break;
+
+    case 4:
+    cout<<"su descuento final fue $200"<< endl;
+    break;
+
+    case 5:
+    cout <<" su descuento fue $59"<< endl;
+    break;
+
+    case 6:
+    cout<<" su descuento fue de $59"<<endl;
+    break;
+
+    case 7:
+    cout <<"su descuent fue $85"<<endl;
+    break;
+
+    case 8:
+    cout <<"su descuento final fue $212"<< endl;
+    break;
+    
+    }
+
+
+    cout<<"gracias por su compra,que tenga un excelente dia"<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+return 0;
+}
+
+
+
+
